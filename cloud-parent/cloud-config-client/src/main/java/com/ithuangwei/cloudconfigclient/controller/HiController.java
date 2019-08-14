84d53a4b36cb476943ae42e81ec9873b4afe11cd
huangwei <745191320@qq.com>
2019-08-14 10:41:39 +0800

    新增：学习《深入理解spring cloud 和微服务构建》第十章开始

---
+++ cloud-parent/cloud-config-client/src/main/java/com/ithuangwei/cloudconfigclient/controller/HiController.java
@@ -0,0 +1,23 @@
+package com.ithuangwei.cloudconfigclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Huangw
+ * @projectName workspace_study
+ * @description: TODO
+ * @date 2019/8/14 10:35
+ */
+@RestController
+public class HiController {
+
+    @Value("${foo}")
+    String foo;
+
+    @GetMapping("/foo")
+    public String hi() {
+        return foo;
+    }
+}
